fc1d9474dc11fabeadcb1c3124dc0d60d194b5e0
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-03-16 17:33:22 +0530

    mongodb url change

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ const passport = require('passport');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(()=>{console.log("MONGODB Connected !!!");})
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  console.log("MONGODB Connected !!!");
+});
 
 
 const postController = require("./controllers/postController");
